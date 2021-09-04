20be48a8c0f8ea2cee855eb36186b975da9c0eeb
d-ivashchuk <dimaiv@me.com>
2021-09-04 22:14:07 +0200

    try fixing the migrations

--- prisma/migrations/20210904084007_add_custom_name_column_to_the_heat/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Heat" ADD COLUMN     "name" TEXT;
--- prisma/migrations/20210904201333_add_new_heats_structure/migration.sql
+++ prisma/migrations/20210904201333_add_new_heats_structure/migration.sql
@@ -3,8 +3,6 @@
 
   - You are about to drop the column `environments` on the `Heat` table. All the data in the column will be lost.
   - You are about to drop the column `users` on the `Heat` table. All the data in the column will be lost.
-  - Added the required column `property` to the `Heat` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `strategy` to the `Heat` table without a default value. This is not possible if the table is not empty.
 
 */
 -- CreateEnum
@@ -16,6 +14,7 @@ ALTER TYPE "HeatType" ADD VALUE 'CUSTOM';
 -- AlterTable
 ALTER TABLE "Heat" DROP COLUMN "environments",
 DROP COLUMN "users",
-ADD COLUMN     "property" TEXT NOT NULL,
-ADD COLUMN     "strategy" "Strategy" NOT NULL,
+ADD COLUMN     "name" TEXT,
+ADD COLUMN     "property" TEXT NOT NULL DEFAULT E'users',
+ADD COLUMN     "strategy" "Strategy" NOT NULL DEFAULT E'IN',
 ADD COLUMN     "values" TEXT[];
